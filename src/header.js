cdaa22e6f75c4e8e2e1c52db842876c3332fd687
Rohan Dafare <121180382+rohandafarefr@users.noreply.github.com>
2023-05-24 18:47:54 +0700

    Add files via upload

---
+++ src/header.js
@@ -0,0 +1,11 @@
+let header = document.getElementById("header")
+header.innerHTML= `
+    <nav id="nav">
+        <a href="#"><i class="fa-solid fa-arrow-left"></i></a>
+        <span class="nav-title">Dine-in Pe</span>
+        <a href="#"><i class="fa-solid fa-circle-user"></i></a>
+        <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+    </nav>
+`
+
+
