f6e6c517c2ab21bb2aa78e7ac197e13167209ea5
Rohan Dafare <121180382+rohandafarefr@users.noreply.github.com>
2023-05-25 00:47:12 +0700

    Add files via upload

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,5 @@
 let footer = document.getElementById("footer")
-footer.innerHTML= `
+footer.innerHTML = `
     <a href="#">
     <span class="foot-subt">Powered By</span>
     <span class="foot-title">OneUIX</span>
--- src/header.js
+++ src/header.js
@@ -1,11 +1,15 @@
 let header = document.getElementById("header")
-header.innerHTML= `
+header.innerHTML = `
     <nav id="nav">
-        <a href="#"><i class="fa-solid fa-arrow-left"></i></a>
-        <span class="nav-title">Dine-in Pe</span>
-        <a href="#"><i class="fa-solid fa-circle-user"></i></a>
-        <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+        <a href="#">
+            <i class="fa-solid fa-arrow-left"></i>
+        </a>
+            <span class="nav-title">Dine-in Pe</span>
+        <a href="#">
+            <i class="fa-solid fa-circle-user"></i>
+        </a>
+        <a href="#">
+            <i class="fa-solid fa-cart-shopping"></i>
+        </a>
     </nav>
 `
-
-
