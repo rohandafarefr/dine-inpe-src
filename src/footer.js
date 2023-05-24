02ee78fa9fd62400c2c2332130c24b266bff1170
Rohan Dafare <121180382+rohandafarefr@users.noreply.github.com>
2023-05-24 23:36:38 +0700

    Add files via upload

---
+++ src/footer.js
@@ -0,0 +1,7 @@
+let footer = document.getElementById("footer")
+footer.innerHTML= `
+    <a href="#">
+    <span class="foot-subt">Powered By</span>
+    <span class="foot-title">OneUIX</span>
+    </a>
+`
